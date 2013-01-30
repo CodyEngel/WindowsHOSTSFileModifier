fca7df982231c00b626cf09e9de96a0b77192bb7
Cody Engel <cengel815@gmail.com>
2013-01-30 17:26:35 -0600

    Commit this to memory.

---
+++ WindowsHOSTFileModifier/HOSTFileModifier.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.bBackup = New System.Windows.Forms.Button()
+        Me.bMalware = New System.Windows.Forms.Button()
+        Me.bRestore = New System.Windows.Forms.Button()
+        Me.bZeuS = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'bBackup
+        '
+        Me.bBackup.Location = New System.Drawing.Point(12, 12)
+        Me.bBackup.Name = "bBackup"
+        Me.bBackup.Size = New System.Drawing.Size(129, 43)
+        Me.bBackup.TabIndex = 0
+        Me.bBackup.Text = "Backup Host File"
+        Me.bBackup.UseVisualStyleBackColor = True
+        '
+        'bMalware
+        '
+        Me.bMalware.Location = New System.Drawing.Point(12, 61)
+        Me.bMalware.Name = "bMalware"
+        Me.bMalware.Size = New System.Drawing.Size(260, 43)
+        Me.bMalware.TabIndex = 1
+        Me.bMalware.Text = "Malware"
+        Me.bMalware.UseVisualStyleBackColor = True
+        '
+        'bRestore
+        '
+        Me.bRestore.Location = New System.Drawing.Point(143, 12)
+        Me.bRestore.Name = "bRestore"
+        Me.bRestore.Size = New System.Drawing.Size(129, 43)
+        Me.bRestore.TabIndex = 2
+        Me.bRestore.Text = "Restore Host File"
+        Me.bRestore.UseVisualStyleBackColor = True
+        '
+        'bZeuS
+        '
+        Me.bZeuS.Location = New System.Drawing.Point(12, 110)
+        Me.bZeuS.Name = "bZeuS"
+        Me.bZeuS.Size = New System.Drawing.Size(260, 43)
+        Me.bZeuS.TabIndex = 3
+        Me.bZeuS.Text = "ZeuS"
+        Me.bZeuS.UseVisualStyleBackColor = True
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.bZeuS)
+        Me.Controls.Add(Me.bRestore)
+        Me.Controls.Add(Me.bMalware)
+        Me.Controls.Add(Me.bBackup)
+        Me.Name = "MainForm"
+        Me.Text = "Host File Modifier"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents bBackup As System.Windows.Forms.Button
+    Friend WithEvents bMalware As System.Windows.Forms.Button
+    Friend WithEvents bRestore As System.Windows.Forms.Button
+    Friend WithEvents bZeuS As System.Windows.Forms.Button
+
+End Class
---
+++ WindowsHOSTFileModifier/HOSTFileModifier.vb
@@ -0,0 +1,91 @@
+﻿Imports System
+Imports System.IO
+Imports System.Text
+Imports System.Collections.Generic
+
+Public Class MainForm
+    ' Set the path of hosts
+    Dim hostsPath As String = System.Environment.GetFolderPath(Environment.SpecialFolder.System) & "\Drivers\etc\"
+    Dim desktop As String = System.Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+    Dim hostsBackupFile = "temphosts"
+    Dim hostsFile = "hosts"
+    Dim hostsUserBackupFile = "WHFMhosts"
+
+    '################################################################################
+    '#          Backup Button
+    '#          Creates a backup of the users hosts file.
+    '#
+    '#          hosts -> WHFMhosts
+    '################################################################################
+    Private Sub bBackup_Click(sender As Object, e As EventArgs) Handles bBackup.Click
+        copyFile(hostsFile, hostsUserBackupFile)
+    End Sub
+
+    Private Sub bRestore_Click(sender As Object, e As EventArgs) Handles bRestore.Click
+        copyFile(hostsUserBackupFile, "hosts")
+    End Sub
+
+    '################################################################################
+    '#          Malware Button
+    '#          Appends the hosts file to block websites known to be infected with
+    '#          malware.
+    '################################################################################
+    Private Sub bMalware_Click(sender As Object, e As EventArgs) Handles bMalware.Click
+        appendHostFile("http://www.malwaredomainlist.com/hostslist/hosts.txt", "MALWARE")
+    End Sub
+
+    '################################################################################
+    '#          ZeuS Button
+    '#          Appends the hosts file to block websites known to be infected with
+    '#          ZeuS.
+    '################################################################################
+    Private Sub bZeuS_Click(sender As Object, e As EventArgs) Handles bZeuS.Click
+        appendHostFile("https://zeustracker.abuse.ch/blocklist.php?download=hostfile", "ZeuS")
+    End Sub
+
+    '################################################################################
+    '#          Copy Hosts File
+    '#          Creates a copy of the hosts file.
+    '################################################################################
+    Private Sub copyFile(source, destination)
+        Dim sourceFileName As String = hostsPath & source
+        Dim destinationFileName As String = hostsPath & destination
+        Dim overwrite As Boolean = True
+
+        ' Try to copy the hosts file to WHFMHosts
+        Try
+            FileSystem.FileCopy(sourceFileName, destinationFileName)
+        Catch
+            ' If the program wasn't run as an administrator it will not work
+            MsgBox("This program must be run as an administrator.", , "Error 37!")
+            Return
+        End Try
+    End Sub
+
+    '################################################################################
+    '#          Append Host File
+    '#          Appends the host file with a list of blocked webistes.
+    '################################################################################
+    Private Sub appendHostFile(downloadFile, infectionName)
+        My.Computer.Network.DownloadFile(downloadFile, desktop & "\blocked-sites.txt", "", "", True, 1000, True)
+        If System.IO.File.Exists(hostsPath & hostsBackupFile) = False Then
+            copyFile(hostsFile, hostsBackupFile)
+        End If
+
+        Dim sb As New StringBuilder()
+        Using sr As New StreamReader(hostsPath & "\hosts")
+            sb.Append(sr.ReadToEnd())
+        End Using
+        Using sr As New StreamReader(desktop & "\blocked-sites.txt")
+            sb.AppendLine("# START OF KNOWN " & infectionName & " INFECTED WEBSITES")
+            sb.Append(sr.ReadToEnd())
+            sb.AppendLine("# END OF KNOWN " & infectionName & " INFECTED WEBSITES")
+        End Using
+
+        Using outfile As New StreamWriter(hostsPath & "hosts")
+            outfile.Write(sb.ToString())
+        End Using
+
+        My.Computer.FileSystem.DeleteFile(desktop & "\blocked-sites.txt")
+    End Sub
+End Class
